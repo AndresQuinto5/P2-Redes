c5a5e29ca6cc8a3b378e00bd12c76451d308d5a5
Andres <qui18288@uvg.edu.gt>
2022-09-28 20:58:33 -0600

    server.js

--- server/server.js
+++ server/server.js
@@ -31,3 +31,18 @@ const addUser = ({ id, name, playerType, room }) => {
   users.push(newUser);
   return { newUser };
 };
+
+const removeUser = (id) => {
+  const removeIndex = users.findIndex((user) => user.id === id);
+
+  if (removeIndex !== -1) return users.splice(removeIndex, 1)[0];
+};
+
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+/**************************************************************** */
