71a96d34084fac4c22e499d466cbdc362502bd84
Andres <qui18288@uvg.edu.gt>
2022-09-28 21:03:16 -0600

    START JS FIXED

--- client/client/src/components/Start.js
+++ client/client/src/components/Start.js
@@ -7,6 +7,58 @@ const Start = () => {
   const [roomCode, setRoomCode] = useState("");
   const [username, setUsername] = useState("");
 
+  return (
+    <div className='Homepage-Container'>
+      <h1 style={{ color: "white" }}>Rack-O - AndresQuinto, Mirka M, Oscar de Leon</h1>
+      <img
+        src='https://m.media-amazon.com/images/I/71ERH-4PeWL._AC_SL1500_.jpg'
+        className='Homepage-background-image'
+      />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "50%",
+          padding: "0 400px",
+          flexWrap: "wrap",
+        }}
+      >
+        <input
+          type='text'
+          placeholder='Username'
+          onChange={(event) => setUsername(event.target.value)}
+          className='Homepage-input'
+          style={{ paddingLeft: "10px" }}
+        />
+        {username !== "" && (
+          <input
+            type='text'
+            placeholder='Game Code'
+            onChange={(event) => setRoomCode(event.target.value)}
+            className='Homepage-input'
+            style={{ paddingLeft: "10px" }}
+          />
+        )}
 
+        {username !== "" && (
+          <Link
+            to={`/racko?roomCode=${uuidv4().slice(0, 8)}&username=${username}`}
+          >
+            <button className='Homepage-button CreateRoom-button'>
+              Create Room
+            </button>
+          </Link>
+        )}
+        {roomCode !== "" && (
+          <Link to={`/racko?roomCode=${roomCode}&username=${username}`}>
+            <button className='Homepage-button JoinRoom-button'>
+              Join Room
+            </button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Start;
