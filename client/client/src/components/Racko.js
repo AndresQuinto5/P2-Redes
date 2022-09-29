590a0eefd0c81246b84d66247bbaeaaed58e8f73
Andres <qui18288@uvg.edu.gt>
2022-09-28 23:15:22 -0600

    racko game

--- client/client/src/components/Racko.js
+++ client/client/src/components/Racko.js
@@ -16,5 +16,124 @@ const shuffle = (array) => {
   return array;
 };
 
+const isAscending = (arr) => {
+  return arr.every(function (x, i) {
+    return i === 0 || x >= arr[i - 1];
+  });
+};
+
+const Racko = (props) => {
+  const data = queryString.parse(props.location.search);
+  const history = useHistory();
+  const [room] = useState(data.roomCode);
+  const [roomFull, setRoomFull] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const [deck, setDeck] = useState(shuffle([...Array(61).keys()]));
+  const [discartedDeck, setDiscartedDeck] = useState([]);
+
+  const [player1Deck, setPlayer1Deck] = useState([]);
+  const [player2Deck, setPlayer2Deck] = useState([]);
+  const [player3Deck, setPlayer3Deck] = useState([]);
+  const [player4Deck, setPlayer4Deck] = useState([]);
+
+  const [gameOver, setGameOver] = useState(false);
+  const [turn, setTurn] = useState("");
+  const [winner, setWinner] = useState("");
+
+  const [isChatBoxHidden, setChatBoxHidden] = useState(true);
+  const [deckPressed, setDeckPressed] = useState(false);
+  const [currentDeckCard, setCurrentDeckCard] = useState("");
+  const [takeDeckCard, setTakeDeckCard] = useState(false);
+  const [deckCardPosition, setDeckCardPosition] = useState(0);
+
+  const toggleChatBox = () => {
+    const chatBody = document.querySelector(".chat-body");
+    if (isChatBoxHidden) {
+      chatBody.style.display = "block";
+      setChatBoxHidden(false);
+    } else {
+      chatBody.style.display = "none";
+      setChatBoxHidden(true);
+    }
+  };
+
+  useEffect(() => {
+    const connectionOptions = {
+      forceNew: true,
+      reconnectionAttempts: "Infinity",
+      timeout: 10000,
+      transports: ["websocket"],
+    };
+    socket = io.connect(ENDPOINT, connectionOptions);
+
+    socket.emit("join", { room, name: data.username }, (error) => {
+      if (error) setRoomFull(true);
+    });
+
+    return () => {
+      try {
+        //cleanup on component unmount
+        socket.emit("disconnect");
+        //shut down connnection instance
+        socket.off();
+      } catch (e) {}
+    };
+  }, []);
+
+  useEffect(() => {
+    setDeckPressed(false);
+  }, [turn]);
+
+  useEffect(() => {
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+
+    socket.on("currentUserData", ({ playerType }) => {
+      setCurrentUser(playerType);
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.emit("initGameState", {
+      gameOver: false,
+      turn: "Player 1",
+      player1Deck: deck.slice(0, 10),
+      player2Deck: deck.slice(10, 20),
+      player3Deck: deck.slice(20, 30),
+      player4Deck: deck.slice(30, 40),
+      deck: deck.slice(40, 60),
+      discartedDeck: [],
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.on(
+      "initGameState",
+      ({
+        gameOver,
+        turn,
+        player1Deck,
+        player2Deck,
+        player3Deck,
+        player4Deck,
+        deck,
+        discartedDeck,
+      }) => {
+        setGameOver(gameOver);
+        setTurn(turn);
+        setPlayer1Deck(player1Deck);
+        setPlayer2Deck(player2Deck);
+        setPlayer3Deck(player3Deck);
+        setPlayer4Deck(player4Deck);
+        setDeck(deck);
+        setDiscartedDeck(discartedDeck);
+      }
+    );
+
 
 export default memo(Racko);
