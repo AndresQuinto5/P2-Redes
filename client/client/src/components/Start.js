7048e78601c2457944b1f8ebb3878122ec2804e6
Mirka <mirkanicolle@gmail.com>
2022-10-04 18:52:18 -0600

    start

--- client/client/src/components/Start.js
+++ client/client/src/components/Start.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import "../styles/start.css";
 
-
 const Start = () => {
   const [roomCode, setRoomCode] = useState("");
   const [username, setUsername] = useState("");
